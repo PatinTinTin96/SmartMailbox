e02d26b65f49067ffb03353c00cfd633336d33aa
Patricio Soto <patricio.soto40@inacapmail.cl>
2020-06-06 21:47:49 -0400

    v1.10 Correcciones de diseño

--- app/src/main/java/com/example/smartmailbox/RegistroUsuario.java
+++ app/src/main/java/com/example/smartmailbox/RegistroUsuario.java
@@ -2,17 +2,32 @@ package com.example.smartmailbox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class RegistroUsuario extends AppCompatActivity {
-
+Button btnRegistrar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+        btnRegistrar=findViewById(R.id.btnRegistrar);
+        btnRegistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registrar();
 
+            }
+        });
 
+    }
+
+    private void registrar() {
+        Intent r = new Intent(RegistroUsuario.this,Principal.class);
+        startActivity(r);
     }
 
 
